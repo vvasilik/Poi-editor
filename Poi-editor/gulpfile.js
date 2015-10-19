f371660c068ca1978af86e4d5fd6aacd5433d294
vvasilik <vasiliyworkplace@gmail.com>
2015-10-19 13:32:12 +0300

    feat & refactor: gulp + classNames

---
+++ Poi-editor/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('css', function () {
+  gulp.src('css/*.css')
+    .pipe(autoprefixer({
+      browsers: ['> 1%'],
+      cascade: false
+    }))
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('bundle'))
+});
+
+gulp.task('js', function () {
+  gulp.src('js/*.js')
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('bundle'));
+});
+
+gulp.task('default', ['css', 'js']);
+
+gulp.task('watch', function () {
+  gulp.watch('**/*.*', ['default'])
+});
