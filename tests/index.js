ec82fd40e3807afc373ea968b0e1745c6b7ee0df
Vasyl Vasylyk <vasiliyworkplace@gmail.com>
2020-03-29 17:13:20 +0200

    add puppeteer

---
+++ tests/index.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('http://127.0.0.1:8080');
+  await page.waitFor(1000);
+  await page.screenshot({path: 'tests/screenshot/home.png'});
+  await page.click('.js-to-map');
+  await page.waitFor(1000);
+  await page.screenshot({path: 'tests/screenshot/map.png'});
+  await browser.close();
+})();
